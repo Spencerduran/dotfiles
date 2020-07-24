75f608b6a5c1e8c4cf500f54d8b7242c09dfb1b3
spencer <spencerduran@rockethomes.com>
2020-07-24 15:39:54 -0400

    default directory for rg and fzf

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,6 @@ status --is-interactive; and source (pyenv init -|psub)
 
 set -g -x NVM_NODEJS_ORG_MIRROR http:/nodejs.org/dist
 set -g -x NODEJS_ORG_MIRROR http://nodejs.org/dist
-set -g -x NODE_TLS_REJECT_UNAUTHORIZED 0
 
 set -gx FZF_DEFAULT_COMMAND  'rg -i --files --no-ignore-vcs --hidden'
 
