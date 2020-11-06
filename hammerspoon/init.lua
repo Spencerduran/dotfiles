ce6714ac327672e002c2021c3a9bf30a226e0713
Spencer Duran <spencerduran@rockethomes.com>
2020-11-06 10:10:54 -0500

    dotFilesCommit

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,8 +1,11 @@
-hs.hotkey.bind({"ctrl"}, "space", function()
+-- Show/hide alacritty with 2x ctrl press
+ctrlDoublePress = require("ctrlDoublePress")
+ctrlDoublePress.timeFrame = 2
+ctrlDoublePress.action = function()
   local alacritty = hs.application.find('alacritty')
   if alacritty:isFrontmost() then
     alacritty:hide()
   else
     hs.application.launchOrFocus("/Applications/Alacritty.app")
   end
-end)
+end
