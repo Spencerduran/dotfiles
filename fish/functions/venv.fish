06e9253114bb94c7174cfcde952a53e3fd82fb9f
Spencer Duran <spencerduran@rockethomes.com>
2020-11-06 09:24:05 -0500

    dotFilesCommit

---
+++ fish/functions/venv.fish
@@ -0,0 +1,11 @@
+function venv
+  deactivate
+  pipenv --rm
+  echo "Creating a Python virtual environment in $PWD"
+  pyenv shell 3.6.8
+  python -m venv .venv
+  source .venv/bin/activate.fish
+  pip install --upgrade pip pipenv
+  make install
+  make install-dev
+end
