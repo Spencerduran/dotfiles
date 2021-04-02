1e81d9f61081e47564c840fccd3e78d69ecfbb70
Spence <spencerduran@rockethomes.com>
2021-04-02 18:41:29 -0400

    dotfilescommit

--- fish/config.fish
+++ fish/config.fish
@@ -3,9 +3,10 @@ set -gx EDITOR nvim
 #set -g -x NVM_NODEJS_ORG_MIRROR http:/nodejs.org/dist
 #set -g -x NODEJS_ORG_MIRROR http://nodejs.org/dist
 
-#set -gx FZF_DEFAULT_COMMAND  'rg -i --files --no-ignore-vcs --hidden --exclude={~/*,.git,.idea,.vscode,.sass-cache,node_modules,build,tmp}'
+#set -gx FZF_DEFAULT_COMMAND  'rg -i --files --no-ignore-vcs --hidden --exclude={.git,.idea,.vscode,node_modules,build,tmp,~/.vim/*}'
 #set -gx FZF_DEFAULT_OPTS '--ansi --preview-window 'right:60%' --preview 'bat --color=always --style=header,grid --line-range :300 {}'
 
+set -gx FZF_DEFAULT_COMMAND 'rg --files --ignore-case --hidden -g "!{.pyenv,.local,.idea,.vscode,tmp,Library,.vim,.git,node_modules,vendor}/*"'
 #set -U -x AWS_CA_BUNDLE '/usr/local/share/ca-certificates/Quicken_Loans_Root_CA.crt'
 set -U -x AWS_SDK_LOAD_CONFIG
 set -U -x AWS_CA_BUNDLE '~/.ssh/qlca.pem'
