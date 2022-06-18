4abbacc9d330376a2d94d1c05b8c80e18028affa
Spence <Spencerduran@gmail.com>
2022-06-18 06:58:53 -0400

    neovim_modular

---
+++ neovim_modular/snippets/all.lua
@@ -0,0 +1,10 @@
+-- luasnip globals
+-- defined by snip_env in setup
+require("luasnip.loaders.from_lua").lazy_load()
+local env = snip_env
+
+return {
+	env.parse("gmail", "Spencerduran@gmail.com"),
+	env.s("date", env.p(os.date, "%Y-%m-%d")),
+	env.s("time", env.p(os.date, "%H:%M")),
+}
