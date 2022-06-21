12a6e2c7d85ef2ef2f838a6a6ebcdf592aa675fc
Spence <Spencerduran@gmail.com>
2022-06-20 23:43:48 -0400

    Add alpha on empty autocmd, delete modular working folder

--- nvim/lua/autocmd.lua
+++ nvim/lua/autocmd.lua
@@ -1,30 +1,31 @@
 local api = vim.api
 local settings = require("user-conf")
+local funx = require("functions")
 
--- when there is no buffer left show Alpha
-vim.api.nvim_create_augroup("alpha_on_empty", { clear = true })
-vim.api.nvim_create_autocmd("User", {
-	pattern = "BDeletePre",
-	group = "alpha_on_empty",
-	callback = function(event)
-		local found_non_empty_buffer = false
-		local buffers = M.get_listed_buffers()
 
-		for _, bufnr in ipairs(buffers) do
-			if not found_non_empty_buffer then
-				local name = vim.api.nvim_buf_get_name(bufnr)
-				local ft = vim.api.nvim_buf_get_option(bufnr, "filetype")
+vim.api.nvim_create_augroup('alpha_on_empty', { clear = true })
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'BDeletePre',
+  group = 'alpha_on_empty',
+  callback = function(event)
+    local found_non_empty_buffer = false
+    buffers = funx.get_listed_buffers()
 
-				if bufnr ~= event.buf and name ~= "" and ft ~= "Alpha" then
-					found_non_empty_buffer = true
-				end
-			end
-		end
+    for _, bufnr in ipairs(buffers) do
+      if not found_non_empty_buffer then
+        local name = vim.api.nvim_buf_get_name(bufnr)
+        local ft = vim.api.nvim_buf_get_option(bufnr, 'filetype')
 
-		if not found_non_empty_buffer then
-			vim.cmd([[:Alpha]])
-		end
-	end,
+        if bufnr ~= event.buf and name ~= '' and ft ~= 'Alpha' then
+          found_non_empty_buffer = true
+        end
+      end
+    end
+
+    if not found_non_empty_buffer then
+      vim.cmd [[:Alpha]]
+    end
+  end,
 })
 
 -- Don't auto comment new line
