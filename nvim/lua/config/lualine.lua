5b9f35cb72069685556aad62ac41bbe13c127883
Spence <Spencerduran@gmail.com>
2022-07-13 11:25:26 -0400

    lualine separators + undotree plugin

--- nvim/lua/config/lualine.lua
+++ nvim/lua/config/lualine.lua
@@ -12,7 +12,10 @@ end
 
 require("lualine").setup({
 	options = {
-		theme = "catppuccin",
+		--theme = "catppuccin",
+		theme = "rose-pine",
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
 		icons_enabled = true,
 		disabled_filetypes = {},
 		always_divide_middle = false,
@@ -36,7 +39,7 @@ require("lualine").setup({
 				file_status = true, -- Displays file status (readonly status, modified status)
 				path = 1, -- 0: Just the filename 1: Relative path 2: Absolute pathath
 				shorting_target = 40, -- Shortens path to leave 40 spaces in the window
-				symbols = { modified = "[]", readonly = " " },
+				symbols = { modified = " [⭐]", readonly = " " },
 			},
 		},
 		lualine_x = {
