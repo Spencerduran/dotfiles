949985464fd6276e70b719664f8e3b0869c84b8e
SpenceDuran <124907053+SpenceDuran@users.noreply.github.com>
2023-05-19 10:26:37 -0400

    autocommit

--- fish/functions/gitcleanremote.fish
+++ fish/functions/gitcleanremote.fish
@@ -1,4 +1,6 @@
-function gitcleanremote
-git branch -r --merged origin/develop | grep -v 'origin/develop$' | sed 's/origin\///' | while read branch; git push origin --delete $branch;
-end
+git branch -r --merged origin/develop | grep -v 'origin/develop$' | sed 's/origin\///' | while read -l branch
+    set branch (string trim -- $branch)
+    if test -n "$branch"
+        git push origin --delete $branch
+    end
 end
