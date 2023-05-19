0cc9cde051bdd7fc06e71a072b27595ea58a0bfc
SpenceDuran <124907053+SpenceDuran@users.noreply.github.com>
2023-05-19 10:31:14 -0400

    autocommit

--- fish/functions/merge_dates.fish
+++ fish/functions/merge_dates.fish
@@ -1,5 +1,17 @@
 #!/usr/bin/env fish
 
+#The script aims to find all branches that have been merged into the origin/develop branch. It performs the following steps:
+##Retrieves a list of branches that have been merged into origin/develop using the git branch -r --merged origin/develop command.
+##Iterates over each merged branch.
+##Trims any leading or trailing whitespace from the branch name.
+##Skips the iteration if the branch is the origin/develop branch itself.
+##Searches for the merge commit of the branch using git log --merges --oneline $branch..origin/develop | tail -1.
+##If a merge commit is found:
+##Extracts the commit hash, date, and author information using git show.
+##Prints a line indicating that the branch was merged into origin/develop on the specified date by the specified author.
+##If no merge commit is found, prints a line indicating that the branch has not been merged into origin/develop.
+##In summary, the script provides an overview of branches merged into the origin/develop branch, displaying the date, branch name, and the person who performed the merge.
+
 # Get all branches that have been merged into develop
 set merged_branches (git branch -r --merged origin/develop)
 
