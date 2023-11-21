a54f94a8807bedfda67f38bc61b8487987cf8645
SpenceDuran <124907053+SpenceDuran@users.noreply.github.com>
2023-11-21 09:04:44 -0500

    autocommit

--- nvim/lua/config/catppuccin.lua
+++ nvim/lua/config/catppuccin.lua
@@ -1,67 +1,35 @@
-local catppuccin = require("catppuccin")
-
-catppuccin.setup({
-	transparent_background = true,
-	term_colors = false,
-	styles = {
-		comments = "italic",
-		functions = "italic",
-		keywords = "italic",
-		strings = "NONE",
-		variables = "italic",
-	},
+local catpuccin = require("catppuccin").setup({
 	integrations = {
-		treesitter = true,
+		notify = true,
+		dashboard = true,
+		which_key = true,
+		neotree = {
+			enabled = true,
+			show_root = false,
+			transparent_panel = false,
+		},
+		gitgutter = true,
+		telescope = true,
+		cmp = true,
 		native_lsp = {
 			enabled = true,
 			virtual_text = {
-				errors = "italic",
-				hints = "italic",
-				warnings = "italic",
-				information = "italic",
+				errors = { "italic" },
+				hints = { "italic" },
+				warnings = { "italic" },
+				information = { "italic" },
 			},
 			underlines = {
-				errors = "underline",
-				hints = "underline",
-				warnings = "underline",
-				information = "underline",
+				errors = { "underline" },
+				hints = { "underline" },
+				warnings = { "underline" },
+				information = { "underline" },
 			},
 		},
-		lsp_trouble = false,
-		cmp = true,
-		lsp_saga = false,
-		gitgutter = false,
-		gitsigns = true,
-		telescope = true,
-		nvimtree = {
-			enabled = true,
-			show_root = false,
-			transparent_panel = false,
-		},
-		neotree = {
-			enabled = false,
-			show_root = false,
-			transparent_panel = false,
-		},
-		which_key = true,
-		indent_blankline = {
-			enabled = true,
-			colored_indent_levels = false,
-		},
-		dashboard = false,
-		neogit = true,
-		vim_sneak = false,
-		fern = false,
-		barbar = false,
-		bufferline = true,
-		markdown = true,
-		lightspeed = true,
-		ts_rainbow = false,
-		hop = false,
-		notify = true,
-		telekasten = true,
-		symbols_outline = true,
 	},
 })
-vim.g.catppuccin_flavour = "frappe" -- latte, frappe, macchiato, mocha
+
+--vim.g.catppuccin_flavour = "frappe" -- latte, frappe, macchiato, mocha
+--vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
+vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
 vim.cmd([[colorscheme catppuccin]])
