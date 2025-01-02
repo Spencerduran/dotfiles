f4407c1d2474fafe35a149b0aa71280d2cdcd358
Spencer Duran <spencerduran@gmail.com>
2025-01-02 17:45:51 -0500

    refactor session 1

--- nvim/lua/config/tokyo.lua
+++
@@ -1,9 +0,0 @@
-local tokyonight = require("tokyonight")
--- Load the colorscheme
-vim.cmd[[colorscheme tokyonight]]
-vim.g.tokyonight_style = "night"
-vim.o.background = "dark"
-
--- Change the "hint" color to the "orange" color, and make the "error" color bright red
-vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
-
--- nvim/lua/config/vimwiki.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_set_var('vimwiki_global_ext', 0)
-vim.cmd([[
-let g:vimwiki_list = [{'path': '/Users/spencer/repos/VimWiki', 'syntax': 'markdown'}]
-let g:vimwiki_table_mappings = 0 "avoid conflict with auto complete
-let g:vimwiki_conceal_pre =1
-let g:vimwiki_autowriteall =1
-]])
