4ca971520f548a03ed6b887aa74383cf9021ca56
Spencerduran <Spencerduran@gmail.com>
2025-01-03 11:43:08 -0500

    toggleterm

--- nvim/lua/custom_script/plugins/vimwiki.lua
+++ nvim/lua/custom_script/plugins/vimwiki.lua
@@ -5,13 +5,18 @@ return {
 			vim.g.vimwiki_list = {
 				{
 					vimwiki_listsyms = "✗○◐●✓",
-					path = "~/wiki/vimwiki/",
+					path = "~/repos/VimWiki/",
 					syntax = "default",
 					ext = ".wiki",
 					template_path = "~/wiki/templates/",
 					template_default = "default_tmp",
 					template_ext = ".html",
 					auto_toc = 1,
+				},
+				{
+					path = "~/repos/wiki_MonsterLG",
+					syntax = "default",
+					ext = ".wiki",
 				},
 			}
 		end,
