262307c2c14a6d735e2f52475d5d72ed4b147947
Spencerduran <Spencerduran@gmail.com>
2025-02-12 23:12:06 -0500

    removed a bunch of old deprecated shit i was holding onto for some reason

--- nvim/lua/custom_script/plugins/treesitter.lua
+++ nvim/lua/custom_script/plugins/treesitter.lua
@@ -1,3 +1,69 @@
+--return {
+--	"nvim-treesitter/nvim-treesitter",
+--	event = { "BufReadPre", "BufNewFile" },
+--	build = ":TSUpdate",
+--	dependencies = {
+--		"windwp/nvim-ts-autotag",
+--	},
+--	config = function()
+--		-- import nvim-treesitter plugin
+--		local treesitter = require("nvim-treesitter.configs")
+--
+--		-- configure treesitter
+--		treesitter.setup({ -- enable syntax highlighting
+--			TSconfig = {
+--				rainbow = {
+--					enable = true,
+--					extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
+--					max_file_lines = 2000, -- Do not enable for files with more than specified lines
+--				},
+--				highlight = {
+--					enable = true,
+--					additional_vim_regex_highlighting = false,
+--				},
+--				-- enable indentation
+--				indent = { enable = true },
+--				-- enable autotagging (w/ nvim-ts-autotag plugin)
+--				autotag = {
+--					enable = true,
+--				},
+--				-- ensure these language parsers are installed
+--				ensure_installed = {
+--					"bash",
+--					"c",
+--					"dockerfile",
+--					"gitignore",
+--					"graphql",
+--					"html",
+--					"javascript",
+--					"json",
+--					"lua",
+--					"norg",
+--					"markdown",
+--					"markdown_inline",
+--					"query",
+--					"sql",
+--					"svelte",
+--					"tsx",
+--					"typescript",
+--					"vim",
+--					"vimdoc",
+--					"yaml",
+--				},
+--				incremental_selection = {
+--					enable = true,
+--					keymaps = {
+--						init_selection = "<C-space>",
+--						node_incremental = "<C-space>",
+--						scope_incremental = false,
+--						node_decremental = "<bs>",
+--					},
+--				},
+--			},
+--		})
+--		vim.treesitter.language.register("markdown")
+--	end,
+--}
 return {
 	"nvim-treesitter/nvim-treesitter",
 	event = { "BufReadPre", "BufNewFile" },
@@ -8,56 +74,51 @@ return {
 	config = function()
 		-- import nvim-treesitter plugin
 		local treesitter = require("nvim-treesitter.configs")
-
 		-- configure treesitter
-		treesitter.setup({ -- enable syntax highlighting
-			TSconfig = {
-				rainbow = {
-					enable = true,
-					extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
-					max_file_lines = 2000, -- Do not enable for files with more than specified lines
-				},
-				highlight = {
-					enable = true,
-					additional_vim_regex_highlighting = false,
-				},
-				-- enable indentation
-				indent = { enable = true },
-				-- enable autotagging (w/ nvim-ts-autotag plugin)
-				autotag = {
-					enable = true,
-				},
-				-- ensure these language parsers are installed
-				ensure_installed = {
-					"bash",
-					"c",
-					"dockerfile",
-					"gitignore",
-					"graphql",
-					"html",
-					"javascript",
-					"json",
-					"lua",
-					"norg",
-					"markdown",
-					"markdown_inline",
-					"query",
-					"sql",
-					"svelte",
-					"tsx",
-					"typescript",
-					"vim",
-					"vimdoc",
-					"yaml",
-				},
-				incremental_selection = {
-					enable = true,
-					keymaps = {
-						init_selection = "<C-space>",
-						node_incremental = "<C-space>",
-						scope_incremental = false,
-						node_decremental = "<bs>",
-					},
+		treesitter.setup({
+			-- Remove the TSconfig nesting
+			rainbow = {
+				enable = true,
+				extended_mode = true,
+				max_file_lines = 2000,
+			},
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+			indent = { enable = true },
+			autotag = {
+				enable = true,
+			},
+			ensure_installed = {
+				"bash",
+				"c",
+				"dockerfile",
+				"gitignore",
+				"graphql",
+				"html",
+				"javascript",
+				"json",
+				"lua",
+				"norg",
+				"markdown",
+				"markdown_inline",
+				"query",
+				"sql",
+				"svelte",
+				"tsx",
+				"typescript",
+				"vim",
+				"vimdoc",
+				"yaml",
+			},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
 				},
 			},
 		})
